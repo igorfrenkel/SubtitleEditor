309c19c3950ba8a5a8e0c1d5d9d81f8f5c8f2328
Igor Frenkel <bmothebes@gmail.com>
2012-10-24 11:01:01 -0400

    Fixed up runner

--- parser/runner.py
+++ parser/runner.py
@@ -7,13 +7,13 @@ from lib.parser import StringParser
 from datetime import timedelta
 
 if __name__ == '__main__':
-    f = open('/Users/igor/.Trash/Musa - The Warrior .sm.srt','r')
+    f = open('/tmp/meow.srt','r')
     string = f.read()
     f.close()
     
     p = StringParser(string)
     p.parse()
-    #p.uptick(timedelta(seconds=5))
+    p.uptick(timedelta(seconds=1))
     f = open ('/tmp/meow.srt', 'w')
     f.write(str(p))
     f.close()
